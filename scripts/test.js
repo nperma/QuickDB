b2d3563fba80dbe24e92cb90e504f53a16b66eac
Nperma <129764133+nperma@users.noreply.github.com>
2025-05-29 16:34:33 +0700

    Update test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,10 +1,9 @@
 import { world,system } from "@minecraft/server";
 import { QuickDB } from "./database.js";
 
-const PlayerDB = new QuickDB("player-db");
-
 world.afterEvents.playerSpawn.subscribe(({initialSpawn:spawn,player}) => {
   if (spawn && PlayerDB.has(player.id)) system.run(() => {
+    const PlayerDB = new QuickDB("player-db");
     PlayerDB.set(player.id, {
       date: Date.now(),
       spawn: { location: player.location, dimension: player.dimension.id }
